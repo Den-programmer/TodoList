b343e50be8c80f54442c3223e5dd6ce4358cbe69
Den-programmer <denis_grigorov@ukr.net>
2020-04-17 19:19:04 +0300

    Prepare project to development

---
+++ src/BLL/redux.js
@@ -0,0 +1,23 @@
+let store = {
+    _state: {
+        todolist: {
+            tasks:[
+                {
+                    id: 1,
+                },
+                {
+                    id: 2,
+                },
+                {   
+                    id: 3,
+                },
+            ]
+        }
+    },
+    dispatch() {
+
+    }
+}
+window.store = store;
+
+export default store;
---
+++ src/components/todo/todo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import classes from './todo.module.css';
+
+const Todo = () => {
+    return(
+        <div className={classes.todo}>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
