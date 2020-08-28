8d87ff8e56a16f4caa9a6523e5fa8bbc11bbf39e
Den-programmer <denisgrigorov094@gmail.com>
2020-08-28 17:32:54 +0300

    Refactored reducer

--- src/BLL/reducers/reducerTodo.js
+++ src/BLL/reducers/reducerTodo.js
@@ -11,19 +11,19 @@ const todolist = {
     tasks: [
         {
             id: 1,
-            title: 'Повторить неповни речення!',
+            title: 'Register in todolist!',
             done: false,
             isEdit: false
         },
         {
             id: 2,
-            title: 'Выучить второй стих!',
+            title: 'Explore the menu functions!',
             done: false,
             isEdit: false
         },
         {
             id: 3,
-            title: 'Выучить стих!',
+            title: 'Start using app!',
             done: false,
             isEdit: false
         },
@@ -32,69 +32,70 @@ const todolist = {
     errorEditText: 'Untitled'
 }
 
-let reducerTodo = (state = todolist, action) => {
-    let stateCopy = { ...state }
+const reducerTodo = (state = todolist, action) => {
+    const stateCopy = { ...state }
     stateCopy.tasks = [...state.tasks]
 
     switch (action.type) {
         case ADD_TASK:
-            let newTask = {
+            const newTask = {
                 id: stateCopy.tasks.length + 1,
                 title: action.taskTitle,
                 done: false,
                 isEdit: false
             }
-            stateCopy.tasks.push(newTask)
-            return stateCopy
-        case DONE_TASK:
-            stateCopy.tasks.forEach(task => {
-                if (task.id === action.taskId) {
-                    if (task.done === false) {
-                        task.done = true
-                    } else {
-                        task.done = false
-                    }
-                }
-            })
-
-            return stateCopy
-        case DELETE_TASK:
-            stateCopy.tasks = stateCopy.tasks.filter(t => {
-                if (t.id !== action.taskID) return true
-            });
-            return stateCopy
-        case DELETE_ALL_TASKS:
-            if (stateCopy.tasks.length > 0) {
-                stateCopy.cashTasks = stateCopy.tasks
+            return {
+                ...state,
+                tasks: [ ...state.tasks, newTask ] 
             }
-            stateCopy.tasks = []
-
-            return stateCopy
+        case DONE_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.map(task => {
+                    if(task.id === action.taskId) return { ...task, done: !task.done }
+                    return task
+                })
+            }
+        case DELETE_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.filter(task => task.id !== action.taskId && true)
+            }
+        case DELETE_ALL_TASKS:
+            return {
+                ...state,
+                cashTasks: stateCopy.tasks.length > 0 && [ ...state.tasks ],
+                tasks: []
+            }
         case RETURN_DELETED_TASKS:
-            stateCopy.tasks = stateCopy.cashTasks
-
-            return stateCopy
+            return {
+                ...state,
+                tasks: [ ...state.cashTasks ]
+            }
         case EDIT_TASKS:
-            stateCopy.tasks.forEach(task => {
-                if (task.id === action.taskId) task.isEdit = true
-            })
-
-            return stateCopy;
+            return {
+                ...state,
+                tasks: state.tasks.map(task => {
+                    if(task.id === action.taskId) return { ...task, isEdit: true } 
+                    return task
+                })
+            }
         case FINISH_EDITING_TASKS:
-            stateCopy.tasks.forEach(task => {
-                if (task.id === action.taskId && task.title === '') {
-                    task.title = stateCopy.errorEditText
-                }
-                task.isEdit = false
-            })
-
-            return stateCopy
+            return {
+                ...state,
+                tasks: state.tasks.map(task => {
+                    if(task.id === action.taskId && task.title === '') return { ...task, title: state.errorEditText, isEdit: false }
+                })
+            }
         case ON_EDIT_INPUT_CHANGE:
-            stateCopy.cashTasks = stateCopy.tasks
-            stateCopy.tasks.forEach(task => {
-                if (task.id === action.taskId) task.title = action.taskValue
-            });
-            return stateCopy
+            return {
+                ...state,
+                cashTasks: [ ...state.tasks ],
+                tasks: state.tasks.map(task => {
+                    if(task.id === action.taskId) return { ...task, title: action.taskValue }
+                    return task 
+                })
+            }
         default:
             return state
     }
@@ -102,29 +103,14 @@ let reducerTodo = (state = todolist, action) => {
 
 /* Action Creators! */
 
-export const addTask = (taskTitle) => {
-    return { type: ADD_TASK, taskTitle: taskTitle }
-}
-export const doneTask = (taskId) => {
-    return { type: DONE_TASK, taskId: taskId }
-}
-export const deleteTask = (taskID) => {
-    return { type: DELETE_TASK, taskID: taskID }
-}
-export const deleteAllTasks = () => {
-    return { type: DELETE_ALL_TASKS }
-}
-export const returnDeletedTasks = () => {
-    return { type: RETURN_DELETED_TASKS }
-}
-export const editTasks = (taskId) => {
-    return { type: EDIT_TASKS, taskId }
-}
-export const finishEditTasks = (taskId, taskValue) => {
-    return { type: FINISH_EDITING_TASKS, taskId, taskValue }
-}
-export const onEditInputChange = (taskValue, taskId) => {
-    return { type: ON_EDIT_INPUT_CHANGE, taskValue, taskId }
-}
+export const addTask = (taskTitle) => ({ type: ADD_TASK, taskTitle })
+export const doneTask = (taskId) => ({ type: DONE_TASK, taskId })
+export const deleteTask = (taskId) => ({ type: DELETE_TASK, taskId })
+export const deleteAllTasks = () => ({ type: DELETE_ALL_TASKS })
+export const returnDeletedTasks = () => ({ type: RETURN_DELETED_TASKS })
+export const editTasks = (taskId) => ({ type: EDIT_TASKS, taskId })
+export const finishEditTasks = (taskId, taskValue) => ({ type: FINISH_EDITING_TASKS, taskId, taskValue })
+export const onEditInputChange = (taskValue, taskId) => ({ type: ON_EDIT_INPUT_CHANGE, taskValue, taskId })
+
 
 export default reducerTodo
