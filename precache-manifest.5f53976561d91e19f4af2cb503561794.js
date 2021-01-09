4cba26c8ee9944b8528d410ce8dbdefd60961341
Den-programmer <denisgrigorov094@gmail.com>
2021-01-09 21:39:55 +0200

    Updates

---
+++ precache-manifest.5f53976561d91e19f4af2cb503561794.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da4c47f617b944cd0b162b5b698da47",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "297d5e43a2cc6a939bd1",
+    "url": "/TodoList/static/css/main.9d5ef643.chunk.css"
+  },
+  {
+    "revision": "9722a9b662bb4d031ffb",
+    "url": "/TodoList/static/js/2.8108d42a.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/TodoList/static/js/2.8108d42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297d5e43a2cc6a939bd1",
+    "url": "/TodoList/static/js/main.32f5329a.chunk.js"
+  },
+  {
+    "revision": "1911fa1df8b046cdab4e",
+    "url": "/TodoList/static/js/runtime-main.35ad5b3c.js"
+  },
+  {
+    "revision": "ab21f4b080cf7be051964f9d4866f966",
+    "url": "/TodoList/static/media/background_1.ab21f4b0.png"
+  },
+  {
+    "revision": "39a8c6af35eb7682146ed0d7d1302141",
+    "url": "/TodoList/static/media/background_2.39a8c6af.png"
+  },
+  {
+    "revision": "60ec3594d959e563ecebc3ebe40e0e4e",
+    "url": "/TodoList/static/media/background_3.60ec3594.png"
+  },
+  {
+    "revision": "821a316fc23f98da03282474dd59edbf",
+    "url": "/TodoList/static/media/background_4.821a316f.png"
+  },
+  {
+    "revision": "757e3307ca5e9fa8a41b15bdc67e8c9b",
+    "url": "/TodoList/static/media/background_5.757e3307.png"
+  },
+  {
+    "revision": "4b859f208baef559253b2dffbb060d88",
+    "url": "/TodoList/static/media/background_6.4b859f20.png"
+  },
+  {
+    "revision": "fdde3701243143a8930d4e5c4031a384",
+    "url": "/TodoList/static/media/background_7.fdde3701.png"
+  },
+  {
+    "revision": "cf599cbac7c3d6b784862562e7f2e38d",
+    "url": "/TodoList/static/media/background_8.cf599cba.png"
+  },
+  {
+    "revision": "c8dc41d3c9184cfdcffccb5da0dbfe1a",
+    "url": "/TodoList/static/media/backgrounds.c8dc41d3.jpg"
+  },
+  {
+    "revision": "846dc009294371b07c75de59bd6d405c",
+    "url": "/TodoList/static/media/editTodoTasks.846dc009.png"
+  },
+  {
+    "revision": "1e33fae6344823655d2b348b6caecccd",
+    "url": "/TodoList/static/media/menu.1e33fae6.jpg"
+  },
+  {
+    "revision": "34151ba122eda2ceeb95395270d4a0c1",
+    "url": "/TodoList/static/media/myTodolist.34151ba1.png"
+  },
+  {
+    "revision": "2d9318986aa4e5dd37f4486777d0be9c",
+    "url": "/TodoList/static/media/sadSmile.2d931898.png"
+  }
+]);
