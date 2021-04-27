8318fc6a9f072c0e5512633b24610e704c723821
Den-programmer <denisgrigorov094@gmail.com>
2021-04-27 15:51:37 +0300

    Installed firebase!

--- src/index.js
+++ src/index.js
@@ -5,5 +5,24 @@ import App from './App'
 import store from './BLL/redux'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import * as firebase from 'firebase'
 
-ReactDOM.render(<BrowserRouter><Provider store={store}><App store={store}/></Provider></BrowserRouter>, document.getElementById('root'))
+const firebaseConfig = {
+    apiKey: "AIzaSyDioGV7RWVAH2K-QMx26QVIcRfbwSWGAVQ",
+    authDomain: "todolist-backend-2d7cd.firebaseapp.com",
+    databaseURL: "https://todolist-backend-2d7cd-default-rtdb.firebaseio.com",
+    projectId: "todolist-backend-2d7cd",
+    storageBucket: "todolist-backend-2d7cd.appspot.com",
+    messagingSenderId: "914156014929",
+    appId: "1:914156014929:web:e415c444545c09e1afd770",
+    measurementId: "G-FFKNZ0N2FG"
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.analitics()
+
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
+        <App store={store} />
+    </Provider>
+</BrowserRouter>, document.getElementById('root'))
