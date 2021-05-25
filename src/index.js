20fe06dd4c03dd8f845d8e6e20cd7df4cb91cb06
Den-programmer <denisgrigorov094@gmail.com>
2021-05-25 21:54:01 +0300

    Gitignore file updated!

--- src/index.js
+++ src/index.js
@@ -7,18 +7,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import firebase from 'firebase/app' 
 import 'firebase/auth'
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDioGV7RWVAH2K-QMx26QVIcRfbwSWGAVQ",
-    authDomain: "todolist-backend-2d7cd.firebaseapp.com",
-    databaseURL: "https://todolist-backend-2d7cd-default-rtdb.firebaseio.com",
-    projectId: "todolist-backend-2d7cd",
-    storageBucket: "todolist-backend-2d7cd.appspot.com",
-    messagingSenderId: "914156014929",
-    appId: "1:914156014929:web:e415c444545c09e1afd770",
-    measurementId: "G-FFKNZ0N2FG"
-}
+import { firebaseConfig } from './firebaseConfig'
 
 firebase.initializeApp(firebaseConfig)
 
