5cc725524be534b321c6d631fee3302f1e0f1c91
Den-programmer <denisgrigorov094@gmail.com>
2021-05-25 18:57:38 +0300

    Updates

---
+++ precache-manifest.c8ab86c21c043e0300b6ba28579e09c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aabf97320c0e83fb73bc6617be4b97f",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "c5693583b06298ffdaae",
+    "url": "/TodoList/static/css/main.b72c4575.chunk.css"
+  },
+  {
+    "revision": "d62f20a6a9705dde386e",
+    "url": "/TodoList/static/js/2.59a0401e.chunk.js"
+  },
+  {
+    "revision": "8d172b4bef6c1c9ab4c495fe7aefb8d2",
+    "url": "/TodoList/static/js/2.59a0401e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5693583b06298ffdaae",
+    "url": "/TodoList/static/js/main.af0579a2.chunk.js"
+  },
+  {
+    "revision": "1911fa1df8b046cdab4e",
+    "url": "/TodoList/static/js/runtime-main.35ad5b3c.js"
+  },
+  {
+    "revision": "c8dc41d3c9184cfdcffccb5da0dbfe1a",
+    "url": "/TodoList/static/media/backgrounds.c8dc41d3.jpg"
+  },
+  {
+    "revision": "846dc009294371b07c75de59bd6d405c",
+    "url": "/TodoList/static/media/editTodoTasks.846dc009.png"
+  },
+  {
+    "revision": "1e33fae6344823655d2b348b6caecccd",
+    "url": "/TodoList/static/media/menu.1e33fae6.jpg"
+  },
+  {
+    "revision": "34151ba122eda2ceeb95395270d4a0c1",
+    "url": "/TodoList/static/media/myTodolist.34151ba1.png"
+  },
+  {
+    "revision": "2d9318986aa4e5dd37f4486777d0be9c",
+    "url": "/TodoList/static/media/sadSmile.2d931898.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TodoList/precache-manifest.ae08673c83808b53d2d5e3e53e1945b4.js"
+  "/TodoList/precache-manifest.c8ab86c21c043e0300b6ba28579e09c2.js"
 );
 
 self.addEventListener('message', (event) => {
