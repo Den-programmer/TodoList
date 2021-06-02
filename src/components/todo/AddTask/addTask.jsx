241e73e7682b0afe70f04c8a3b46d47024dd7357
Den-programmer <denisgrigorov094@gmail.com>
2021-06-02 22:22:32 +0300

    KeyPress, now you can add task by using 'enter' key!

--- src/components/todo/AddTask/addTask.jsx
+++ src/components/todo/AddTask/addTask.jsx
@@ -1,93 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './addTask.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt, faUndo } from '@fortawesome/free-solid-svg-icons'
 
-class AddTask extends React.Component {
-    state = {
-        undo: {
-            display: 'none'
-        },
-        taskTitle: '',
-        error: false
-    }
+const AddTask = (props) => {
+    const dispNone = { display: 'none' }
+    const dispBlock = { display: 'block' }
 
-    addTask = () => {
-        if (this.state.taskTitle === '') {
-            this.setState({
-                error: true
-            })
+    const [undo, setUndoStatus] = useState(dispNone)
+    const [taskTitle, setTaskTitle] = useState('')
+    const [error, setError] = useState(false)
+
+    const addTask = () => {
+        if (taskTitle === '') {
+            setError(true)
         } else {
-            this.setState({
-                error: false,
-                taskTitle: '' 
-            })
-            this.props.updateAddedTask(this.state.taskTitle)
+            props.updateAddedTask(taskTitle)
+            setError(false)
+            setTaskTitle('')
         }
     }
-
-    onTaskTitleChange = (e) => {
-        this.setState({
-            taskTitle: e.currentTarget.value
-        })
-    }
-
-    deleteAllTasks = () => {
-        this.setState({
-            undo: {
-                display: 'block'
+    useEffect(() => {
+        const KeyPress = (e) => {
+            if (e.key === "Enter") {
+                addTask()
             }
-        })
-        this.props.updateDeletedAllTasks()
+        }
+
+        document.addEventListener('keypress', KeyPress)
+
+        return () => {
+            document.removeEventListener('keypress', KeyPress)
+        }
+    })
+
+    const onTaskTitleChange = (e) => setTaskTitle(e.currentTarget.value)
+
+    const deleteAllTasks = () => {
+        setUndoStatus(dispBlock)
+        props.updateDeletedAllTasks()
     }
-    returnPrevTasks = () => {
-        this.setState({
-            undo: {
-                display: 'none'
-            }
-        })
-        this.props.updateUndoDeletedTasks()
+    const returnPrevTasks = () => {
+        setUndoStatus(dispNone)
+        props.updateUndoDeletedTasks()
     }
-    render() {
-        return (
-            <div className={classes.addTask}>
-                <div className={classes.container}>
-                    <div className={classes.row}>
-                        <div className={classes.title}>
-                            <h4>Your Task:</h4>
-                        </div>
-                        <div>
-                            <input onChange={this.onTaskTitleChange} value={this.state.taskTitle} title="Enter your task!"
-                                ref={ this.addTaskInput }
-                                className={classes.input}
-                                placeholder=" What do you need to do? "
-                                type="text" />
-                        </div>
+    return (
+        <div className={classes.addTask}>
+            <div className={classes.container}>
+                <div className={classes.row}>
+                    <div className={classes.title}>
+                        <h4>Your Task:</h4>
                     </div>
-                    <div className={classes.unitingFlex}>
-                        <div className={classes.managePanel}>
-                            <div className={classes.deleteAllTasks}>
-                                <div>
-                                    <FontAwesomeIcon title="Remove all tasks!" onClick={this.deleteAllTasks} className={classes.trashCan} icon={faTrashAlt} />
-                                </div>
-                                <div style={{ display: this.state.undo.display }}>
-                                    <FontAwesomeIcon title="Restore deleted tasks!" onClick={this.returnPrevTasks} className={classes.undo} icon={faUndo} />
-                                </div>
+                    <div>
+                        <input onChange={onTaskTitleChange} value={taskTitle} title="Enter your task!"
+                            className={classes.input}
+                            placeholder=" What do you need to do? "
+                            type="text" />
+                    </div>
+                </div>
+                <div className={classes.unitingFlex}>
+                    <div className={classes.managePanel}>
+                        <div className={classes.deleteAllTasks}>
+                            <div>
+                                <FontAwesomeIcon title="Remove all tasks!" onClick={deleteAllTasks} className={classes.trashCan} icon={faTrashAlt} />
+                            </div>
+                            <div style={{ display: undo.display }}>
+                                <FontAwesomeIcon title="Restore deleted tasks!" onClick={returnPrevTasks} className={classes.undo} icon={faUndo} />
                             </div>
                         </div>
-                        {this.state.error && <div className={classes.error}>
-                            <p>Enter task's title!</p>
-                        </div>}
-                        <div className={classes.btn_addTask}>
-                            <button onClick={this.addTask} title="Add new item!">
-                                Add!
-                            </button>
-                        </div>
+                    </div>
+                    {error && <div className={classes.error}>
+                        <p>Enter task's title!</p>
+                    </div>}
+                    <div className={classes.btn_addTask}>
+                        <button onClick={addTask} title="Add new item!">
+                            Add!
+                        </button>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default AddTask
